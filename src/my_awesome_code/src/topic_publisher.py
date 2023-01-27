e8fd13de9ea9542c4b8eb1700cfa2886bf74acdc
gianvstheworld <gianlucacapezzuto@usp.br>
2023-01-27 11:39:35 -0300

    'counter' topic done

--- src/my_awesome_code/src/topic_publisher.py
+++ src/my_awesome_code/src/topic_publisher.py
@@ -4,3 +4,13 @@ import rospy
 
 from std_msgs.msg import Int32
 
+rospy.init_node('topic_publisher')
+
+pub = rospy.Publisher('counter', Int32)
+rate = rospy.Rate(2)
+
+count = 0
+while not rospy.is_shutdown():
+    count += 1
+    pub.publish(count)
+    rate.sleep()
